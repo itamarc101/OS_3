9599a051b97612ea73848eb54308ceb008ceaf77
itamarc101 <45041126+itamarc101@users.noreply.github.com>
2023-05-14 03:25:07 +0300

    Update naor.c

--- naor.c
+++ naor.c
@@ -61,89 +61,7 @@ unsigned char calculate_file_checksum(const char *filename)
     return checksum;
 }
 
-
-void client(const char *ip, int port){
-    int sockfd = socket(AF_INET, SOCK_STREAM, 0);
-    if (sockfd < 0)
-    {
-        perror("ERROR opening socket");
-        exit(1);
-    }
-
-    struct sockaddr_in serv_addr;
-    memset(&serv_addr, '0', sizeof(serv_addr));
-    serv_addr.sin_family = AF_INET;
-    serv_addr.sin_port = htons(port);
-    if (inet_pton(AF_INET, ip, &serv_addr.sin_addr) <= 0)
-    {
-        perror("ERROR invalid address");
-        exit(1);
-    }
-
-    if (connect(sockfd, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0)
-    {
-        perror("ERROR connecting");
-        exit(1);
-    }
-    printf("Connected to server %s:%d\n", ip, port);
-
-    struct pollfd fds[2];
-    fds[0].fd = STDIN_FILENO;
-    fds[0].events = POLLIN;
-    fds[1].fd = sockfd;
-    fds[1].events = POLLIN | POLLHUP;
-
-    char buffer[BUFFER_SIZE];
-    while (1)
-    {
-        int ret = poll(fds, 2, 500); // Add timeout of 500 milliseconds
-        if (ret < 0)
-        {
-            perror("ERROR polling");
-            break;
-        }
-        else if (ret == 0)
-        {
-            // Timeout expired and no events to report
-            continue;
-        }
-
-        if (fds[0].revents & POLLIN)
-        {
-            memset(buffer, 0, BUFFER_SIZE);
-            fgets(buffer, BUFFER_SIZE - 1, stdin);
-            if (write(sockfd, buffer, strlen(buffer)) < 0)
-            {
-                perror("ERROR writing to socket");
-                break;
-            }
-        }
-
-
-        if (fds[1].revents & POLLIN)
-        {
-            memset(buffer, 0, BUFFER_SIZE);
-            if (read(sockfd, buffer, BUFFER_SIZE - 1) < 0)
-            {
-                perror("ERROR reading from socket");
-                break;
-            }
-            printf("Server sent: %s", buffer);
-        }
-
-        if (fds[1].revents & POLLHUP)
-        {
-            printf("Connection closed from server side\n");
-            close(sockfd);
-            break;
-        }
-    }
-
-    close(sockfd);
-}
-
-
-void client_ipv4_tcp(int port,const char* ip)
+void client_ipv4_tcp(int port, const char* ip)
 {
     fflush(stdout);
     int sock_addr = socket(AF_INET, SOCK_STREAM,0);
@@ -469,14 +387,6 @@ void server_ipv6_udp(int port, int quiet)
     close(sock_addr);
 }
 
-//&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&7
-//&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&
-//&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&
-//&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&
-//&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&
-//&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&
-
-
 void server_udsdgram()
 {
     printf("TODO!!!\n"); // TODO !!!!!
@@ -544,9 +454,37 @@ void server_mmap(char *filename, int quiet)
 
 }
 
-void client_mmap(char *argv[])
+void client_mmap(char *filename)
 {
-    printf("\n");
+    int fd = open("mmap", O_RDWR, S_IRUSR | S_IWUSR);
+    if (fd == -1)
+    {
+        perror("err");
+        exit(1);
+    }
+
+    char *mmmap = mmap(NULL,SIZE,PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
+    if ( mmmap == MAP_FAILED)
+    {
+        perror("mmap err");
+    }
+
+    char buff[BUFFER_SIZE];
+    FILE * f = fopen(filename, "rb");
+    if ( !f)
+    {
+        perror("open err");
+        exit(1);
+    }
+
+    while(fread(buff, 1 , BUFFER_SIZE, f) > 0) memcpy(mmmap, buff, BUFFER_SIZE);
+    
+    if (munmap(mmmap, SIZE) == -1)
+    {
+        perror("munmap err");
+        exit(1);
+    }
+    close(fd);
 }
 
 void client_pipe(char *filename)
@@ -579,7 +517,7 @@ void client_pipe(char *filename)
     fclose(f);
 }
 
-void server_pipe(int quiet)
+void server_pipe(char *filename, int quiet)
 {
     int fd;
     FILE * f;
@@ -609,18 +547,139 @@ void server_pipe(int quiet)
     fclose(f);
 }
 
-//&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&
-//&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&
-//&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&
-//&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&
-
-
-void server(int port, int perf, int quiet)
+void client(const char *ip, int port)
 {
     int sockfd = socket(AF_INET, SOCK_STREAM, 0);
     if (sockfd < 0)
     {
-        perror("ERROR socket");
+        perror("ERROR opening socket");
+        exit(1);
+    }
+
+    struct sockaddr_in serv_addr;
+    memset(&serv_addr, 0, sizeof(serv_addr));
+
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_port = htons(port);
+    serv_addr.sin_addr.s_addr = inet_addr(ip);
+    if (inet_pton(AF_INET, ip, &serv_addr.sin_addr) <= 0)
+    {
+        perror("ERROR invalid address");
+        exit(1);
+    }
+
+    if (connect(sockfd, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0)
+    {
+        perror("ERROR connecting");
+        exit(1);
+    }
+    printf("Connected to server %s:%d\n", ip, port);
+
+    struct pollfd fds[2];
+    fds[0].fd = STDIN_FILENO;
+    fds[0].events = POLLIN;
+    fds[0].revents = 0;
+    fds[1].fd = sockfd;
+    fds[1].events = POLLIN | POLLHUP;
+    fds[1].revents = 0;
+
+    char buffer[BUFFER_SIZE];
+    while (1)
+    {
+        int ret = poll(fds, 2, 500); // Add timeout of 500 milliseconds
+        if (ret < 0)
+        {
+            perror("ERROR polling");
+            break;
+        }
+        else if (ret == 0)
+        {
+            // Timeout expired and no events to report
+            continue;
+        }
+
+        if (fds[0].revents & POLLIN)
+        {
+            memset(buffer, 0, BUFFER_SIZE);
+            fgets(buffer, BUFFER_SIZE - 1, stdin);
+            if (write(sockfd, buffer, strlen(buffer)) < 0)
+            {
+                perror("ERROR writing to socket");
+                break;
+            }
+        }
+
+
+        if (fds[1].revents & POLLIN)
+        {
+            memset(buffer, 0, BUFFER_SIZE);
+            if (read(sockfd, buffer, BUFFER_SIZE - 1) < 0)
+            {
+                perror("ERROR reading from socket");
+                break;
+            }
+            printf("Server sent: %s", buffer);
+        }
+
+        if (fds[1].revents & POLLHUP)
+        {
+            printf("Connection closed from server side\n");
+            close(sockfd);
+            break;
+        }
+    }
+
+    close(sockfd);
+}
+
+
+
+void client_perf(char* argv[])
+{
+    int port = atoi(argv[2]);
+    int sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    if(sockfd == -1)
+    {
+        perror("err sock");
+        exit(1);
+    }
+
+    struct sockaddr_in serv_addr;
+    memset(&serv_addr, 0, sizeof(serv_addr));
+
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_addr.s_addr = inet_addr("127.0.0.1");
+
+    if(connect(sockfd, (struct sockaddr*)&serv_addr, sizeof(serv_addr))< 0)
+    {
+        perror("err connect");
+        exit(1);
+    }
+
+    if(strcmp(argv[5],"ipv4") == 0)
+    {
+        if(strcmp(argv[6], "tcp") == 0) client_ipv4_tcp(port,argv[2]);
+        else client_ipv4_udp(port, argv[2]);
+    }
+    else if(strcmp(argv[5], "ipv6")==0)
+    {
+        if(strcmp(argv[6], "tcp") == 0) client_ipv6_tcp(port,argv[2]);
+        else client_ipv6_udp(port, argv[2]);
+    }
+
+    else if(strcmp(argv[5], "mmap") == 0) client_mmap(argv[6]);
+    else if(strcmp(argv[5], "pipe") == 0) client_pipe(argv[6]);
+
+}
+
+
+
+void server(int port)
+{
+    int sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    if (sockfd < 0)
+    {
+        perror("ERROR opening socket");
         exit(1);
     }
 
@@ -630,172 +689,244 @@ void server(int port, int perf, int quiet)
         perror("ERROR setting socket options");
         exit(0);
     }
-
     struct sockaddr_in serv_addr;
-    memset(&serv_addr, 0, sizeof(serv_addr));
+    memset(&serv_addr, '0', sizeof(serv_addr));
     serv_addr.sin_family = AF_INET;
     serv_addr.sin_addr.s_addr = INADDR_ANY;
     serv_addr.sin_port = htons(port);
 
     if (bind(sockfd, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0)
     {
-        perror("ERROR binding");
+        perror("ERROR on binding");
         exit(1);
     }
 
-    if (listen(sockfd, 1) < 0)
+    if (listen(sockfd, 5) < 0)
     {
-        perror("ERROR listening");
+        perror("ERROR on listening");
         exit(1);
     }
-   // printf("Server is listening on port %d\n", port);
+    printf("Server is listening on port %d\n", port);
 
-    int confd = accept(sockfd, NULL, NULL);
-    if (confd == -1)
+    while (1)
     {
-        perror("ERROR accepting");
-        exit(1);
+        int clientfd = accept(sockfd, NULL, NULL);
+        if (clientfd < 0)
+        {
+            perror("ERROR on accept");
+            exit(1);
+        }
+        printf("Client connected!\n");
+
+        struct pollfd fds[2];
+        fds[0].fd = STDIN_FILENO;
+        fds[0].events = POLLIN;
+        fds[1].fd = clientfd;
+        fds[1].events = POLLIN;
+
+        char buffer[BUFFER_SIZE];
+        while (1)
+        {
+            if (poll(fds, 2, -1) < 0)
+            {
+                perror("ERROR polling");
+                exit(1);
+            }
+
+            if (fds[0].revents & POLLIN)
+            {
+                memset(buffer, 0, BUFFER_SIZE);
+                fgets(buffer, BUFFER_SIZE - 1, stdin);
+                if (write(clientfd, buffer, strlen(buffer)) < 0)
+                {
+                    perror("ERROR writing to socket");
+                    exit(1);
+                }
+            }
+
+            if (fds[1].revents & POLLIN)
+            {
+                memset(buffer, 0, BUFFER_SIZE);
+                ssize_t n = read(clientfd, buffer, BUFFER_SIZE - 1);
+                if (n < 0)
+                {
+                    perror("ERROR reading from socket");
+                    exit(1);
+                }
+                else if (n == 0)
+                {
+                    printf("Client disconnected\n");
+                    close(clientfd);
+                    break;
+                }
+                printf("Client sent: %s", buffer);
+            }
+
+            if (fds[1].revents & POLLHUP)
+            {
+                printf("client disconnected\n");
+                close(clientfd);
+                break;
+            }
+
+            if (poll(fds, 2, 0) == 0)
+            { // check if timeout expired and no events to report
+                if (clientfd < 0)
+                { // if the socket is closed, the client has disconnected
+                    printf("client disconnected\n");
+                    break;
+                }
+            }
+        }
     }
 
-    char type[BUFFER_SIZE];
-    char param[BUFFER_SIZE];
-
-    memset(type, 0, sizeof(type));
-    memset(param, 0, sizeof(param));
-    read(sockfd, type, BUFFER_SIZE);
-    read(sockfd, param, BUFFER_SIZE);
     close(sockfd);
-    if(strcmp(param, "tcp") == 0)
+}
+
+void server_perf(char* argv[], int quiet)
+{
+    int port = atoi(argv[3]);
+    int sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    if (sockfd < 0)
     {
-        if(strcmp(type,"ipv4") == 0) server_ipv4_tcp(port, quiet);
-        else if(strcmp(type,"ipv6") == 0) server_ipv6_tcp(port, quiet);
-    }
-    else if(strcmp(param,"udp") == 0)
-    {
-        if(strcmp(type,"ipv4") == 0) server_ipv4_udp(port, quiet);
-        else if(strcmp(type,"ipv6") == 0) server_ipv6_udp(port, quiet);
-    }
-    else if(strcmp(type, "uds") == 0)
-    {
-        if(strcmp(param, "stream") == 0) server_udsstream();
-        else if(strcmp(param, "dgram") == 0) server_udsdgram();
-        printf("need to fulfill");
-    }
-    else if(strchr(param, '.') != NULL)
-    {
-        if(strcmp(type, "mmap") == 0) server_mmap(filename, quiet);
-        else if(strcmp(type, "pipe") == 0) server_pipe();
+        perror("ERROR opening socket");
+        exit(1);
     }
 
+    int enable = 1;
+    if (setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &enable, sizeof(int)) < 0)
+    {
+        perror("ERROR setting socket options");
+        exit(0);
+    }
+    struct sockaddr_in serv_addr;
+    memset(&serv_addr, 0, sizeof(serv_addr));
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_addr.s_addr = INADDR_ANY;
+    serv_addr.sin_port = htons(atoi(argv[3]));
 
-////////////////////////////////////////////////
-    // while (1)
-    // {
-    //     int clientfd = accept(sockfd, NULL, NULL);
-    //     if (clientfd < 0)
-    //     {
-    //         perror("ERROR on accept");
-    //         exit(1);
-    //     }
-    //     printf("Client connected!\n");
+    if (bind(sockfd, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0)
+    {
+        perror("ERROR on binding");
+        exit(1);
+    }
 
-    //     struct pollfd fds[2];
-    //     fds[0].fd = STDIN_FILENO;
-    //     fds[0].events = POLLIN;
-    //     fds[1].fd = clientfd;
-    //     fds[1].events = POLLIN;
+    if (listen(sockfd, 5) < 0)
+    {
+        perror("ERROR on listening");
+        exit(1);
+    }
+    
+    struct sockaddr_in client_addr;
+    socklen_t client_addr_size = sizeof(client_addr);
+    int client_sock = accept(sockfd, (struct sockaddr*) & client_addr, &client_addr_size);
+    if(client_sock == -1)
+    {
+        perror("err sock accept");
+        exit(1);
+    }
+    char buff[BUFFER_SIZE];
+    int bytes = recv(client_sock, &buff, sizeof(buff), 0);
+    if(bytes == -1){
+        perror("err recv");
+        exit(1);
+    }
 
-    //     char buffer[BUFFER_SIZE];
-    //     while (1)
-    //     {
-    //         if (poll(fds, 2, -1) < 0)
-    //         {
-    //             perror("ERROR polling");
-    //             exit(1);
-    //         }
+    buff[bytes] = '\0';
 
-    //         if (fds[0].revents & POLLIN)
-    //         {
-    //             memset(buffer, 0, BUFFER_SIZE);
-    //             fgets(buffer, BUFFER_SIZE - 1, stdin);
-    //             if (write(clientfd, buffer, strlen(buffer)) < 0)
-    //             {
-    //                 perror("ERROR writing to socket");
-    //                 exit(1);
-    //             }
-    //         }
+    if(strcmp(buff,"ipv4 tcp") == 0)
+    {
+        server_ipv4_tcp(port + 1, quiet);
+        return;
+    }
 
-    //         if (fds[1].revents & POLLIN)
-    //         {
-    //             memset(buffer, 0, BUFFER_SIZE);
-    //             ssize_t n = read(clientfd, buffer, BUFFER_SIZE - 1);
-    //             if (n < 0)
-    //             {
-    //                 perror("ERROR reading from socket");
-    //                 exit(1);
-    //             }
-    //             else if (n == 0)
-    //             {
-    //                 printf("Client disconnected\n");
-    //                 close(clientfd);
-    //                 break;
-    //             }
-    //             printf("Client sent: %s", buffer);
-    //         }
+    else if(strcmp(buff,"ipv4 udp") == 0)
+    {
+        server_ipv4_udp(port + 1, quiet);
+        return;
+    }
 
-    //         if (fds[1].revents & POLLHUP)
-    //         {
-    //             printf("client disconnected\n");
-    //             close(clientfd);
-    //             break;
-    //         }
+    else if(strcmp(buff,"ipv6 tcp") == 0)
+    {
+        server_ipv6_tcp(port + 1, quiet);
+        return;
+    }
 
-    //         if (poll(fds, 2, 0) == 0)
-    //         { // check if timeout expired and no events to report
-    //             if (clientfd < 0)
-    //             { // if the socket is closed, the client has disconnected
-    //                 printf("client disconnected\n");
-    //                 break;
-    //             }
-    //         }
-    //     }
-    // }
+    else if(strcmp(buff,"ipv6 udp") == 0)
+    {
+        server_ipv6_udp(port + 1, quiet);
+        return;
+    }
+    
+    else if(strcmp(buff, "uds stream") == 0)
+    {
+        server_udsstream(quiet);
+        return;
+    }
 
+    else if(strcmp(buff, "uds dgram") == 0)
+    {
+        server_udsdgram(quiet);
+        return;
+    }
+
+    else if(strcmp(buff, "mmap") == 0)
+    {
+        char *filename = "mmap";
+        server_mmap(filename, quiet);
+        return;
+    }
+
+    else if(strcmp(buff, "pipe") == 0)
+    {
+        char *filename = "pipe";
+        server_mmap(filename, quiet);
+        return;
+    }
+  
 }
 
 int main(int argc, char *argv[])
 {
     int perf = 0, quiet = 0;
+
     if (argc < 3 || argc > 7)
     {
-        printf("Usage: %s [-c IP PORT (opt: -p type param |-s PORT (opt: -p -q)]\n", argv[0]);
+        printf("Usage: %s [-c IP PORT|-s PORT]\n", argv[0]);
         exit(1);
     }
-    int is_client = 0;
-    const char *ip;
-    int port;
 
     for (int i = 0; i < argc; i++)
     {
         if (strcmp(argv[i], "-p") == 0) perf = 1;
         else if (strcmp(argv[i], "-q") == 0) quiet = 1;
     }
-    
+
+    int is_client = 0;
+    const char *ip;
+    int port;
+    char * type;
+    char * param;
 
     if (strcmp(argv[1], "-c") == 0)
     {
         is_client = 1;
         ip = argv[2];
         port = atoi(argv[3]);
+
+        if(!perf)
+        {
+            client(ip, port);
+            return 0;
+        }
+        client_perf(argv);
+        return 0;
+
     }
     else if (strcmp(argv[1], "-s") == 0)
     {
         is_client = 0;
         port = atoi(argv[2]);
-        
-        if(argc > 4) if (strcmp(argv[4], "-q") == 0) quiet = 1;
-        if(argc > 3) if (strcmp(argv[3], "-p") == 0) perf = 1;
-
     }
     else
     {
@@ -805,32 +936,12 @@ int main(int argc, char *argv[])
 
     if (is_client)
     {
-        generate_file();
-        if(argv[4] && argv[5])
-        {
-            if(strcmp(argv[4], "ipv4") == 0 ){
-                if(strcmp(argv[5], "tcp") == 0) client_ipv4_tcp(port, ip);
-                else if(strcmp(argv[5], "udp") == 0) client_ipv4_udp(port, ip);
-            }
-            else if(strcmp(argv[4], "ipv6") == 0 ){
-                if(strcmp(argv[5], "tcp") == 0) client_ipv6_tcp(port, ip); 
-                else if(strcmp(argv[5], "udp") == 0) client_ipv4_udp(port, ip);
-            }
-            else if(strcmp(argv[5],"uds")==0)
-            {
-                if(strcmp(argv[6],"stream") == 0) client_udsstream();
-                else client_udsdgram();
-            }
-            else if(strcmp(argv[5],"mmap") == 0) client_mmap();
-            else if(strcmp(argv[5],"pipe") == 0) client_pipe();
-
-        }
         client(ip, port);
     }
     else
     {
-        
-        server(port, perf, quiet);
+        if( !perf) server(port);
+        else server_perf(argv, quiet);
     }
 
     return 0;
